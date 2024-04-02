c17cc8221f2c240a9a26af07ff8abf1177935315
Ob55 <brian55mwangi@gmail.com>
2024-04-02 15:35:22 +0300

    Done assigment

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+from werkzeug.wrappers import Request, Response
+
+@Request.application
+def application(request):
+  print(f'This web is runing a {request.remote_addr}')
+  return Response('A WSGI generated this response!')
+
+
+if __name__ == '--main--':
+  from werkzeug.serving import run_simple
+  run_simple(
+    hostname = 'localhost',
+    port= 5555,
+    application=application
+  )
+  
